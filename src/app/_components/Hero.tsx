b9ce7ed753b409a3c2794c54161bc2070ba9056c
Kako Ferrare <kakoferrare87@gmail.com>
2025-03-28 14:11:10 -0300

    feat: images

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -28,3 +28,34 @@ export default function Hero() {
         </section >
     );
 }
+
+/*import { useTranslations } from 'next-intl';
+import background from '../../../public/imgs/back6.png'
+import Image from 'next/image';
+
+export default function Hero() {
+    const t = useTranslations('Hero');
+    return (
+        <section className="bg-black relative overflow-hidden">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                <div className='flex flex-col items-center justify-center'>
+                    <h1 className="relative pt-8 text-white text-5xl font-bold">Coders Group</h1>
+                    <p className="relative text-white pt-3">
+                        {t('subtitle')}
+                    </p>
+                </div>
+                <div className="relative w-full h-80 hidden lg:block">
+                    <Image
+                        src={background}
+                        alt='background'
+                        className='object-cover opacity-40 border-8 rounded-4xl p-10'
+                        fill
+                        quality={100}
+                        priority
+                    />
+                </div>
+            </div>
+
+        </section >
+    );
+}*/
